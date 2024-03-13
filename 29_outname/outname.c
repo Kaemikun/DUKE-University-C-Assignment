6ac12de9d871fe85d671ac11b9390f35f5e8843a
Coursera Learner <nobody@nowhere.nul>
2024-03-13 15:36:55 +0000

    Done Assignemnt 29

--- 29_outname/outname.c
+++ 29_outname/outname.c
@@ -2,7 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include "outname.h"
-
+/*
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+}*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "outname.h"
+
+char * computeOutputFileName(const char * inputName) {
+  char * outname = malloc((strlen(inputName)+4)*sizeof(*outname));
+  strcpy(outname , inputName);
+  outname= realloc(outname ,(strlen(inputName)+10)*sizeof(*outname));
+  strcat(outname,".counts");
+  return outname;
+  //WRITE ME
 }
+
